c859a7e7ffe8be88af95bb6418a7e741b7e1e281
myselfAg <agneeshpersonal@gmail.com>
2025-08-09 20:46:31 +0530

    Customer Management System

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/read/:id" element={<Read />} />
+          
 
         </Routes>
       </BrowserRouter>
